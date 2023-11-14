c1fbd9399a46b3c9a9132d114258c03e43dd0361
texasjkee <gt.gluck@gmail.com>
2023-11-14 20:25:46 +0200

    add: syntax

--- syntax/boolean.sol
+++ syntax/boolean.sol
@@ -2,20 +2,20 @@
 pragma solidity ^0.8;
 
 contract Demo {
-    // bool public myBool = true; //state
-    // unsigned integers
-    // uint public myUnit = 42;
-    // uint8 public mySmallUnit = 2;
-    // 2 ** 8 = 256; 
-    // 0 --> (256-1)
-    // step 8 
-    //uint16
-    //uint24
-    //uint32
-    // int public myInt = -42;
-    // int public mySmallInt = -42;
-    // 2 ** 7 = 128
-    // -128 --> (128-1)
+     bool public myBool = true; //state
+     unsigned integers
+     uint public myUnit = 42;
+     uint8 public mySmallUnit = 2;
+     2 ** 8 = 256; 
+     0 --> (256-1)
+     step 8 
+     uint16
+     uint24
+     uint32
+     int public myInt = -42;
+     int public mySmallInt = -42;
+     2 ** 7 = 128
+     -128 --> (128-1)
 
     // signed integers
 
